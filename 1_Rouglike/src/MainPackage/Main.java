669597319b9cb059de65a7a7bf288bf1ed26fe5e
BigBoss <BigBoss@192.168.0.12>
2016-05-24 20:32:12 +0200

    Switch to mainPackage

---
+++ 1_Rouglike/src/MainPackage/Main.java
@@ -0,0 +1,16 @@
+package MainPackage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hello();
+	}
+	
+	static void hello(){
+		System.out.println("HI");
+	}
+	static void iAmChanged(){
+		System.out.println("Another commit");
+	}
+}
